bbd4b0d13df8fda22a26df6437a01fdc0528983b
pragya5 <pragya.singh.mk@gmail.com>
2014-04-18 20:20:47 -0700

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by Pragya Singh on 4/18/14.
+//  Copyright (c) 2014 Pragya Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
